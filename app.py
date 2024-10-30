df30b2fc8374f216337f7c1c9a87d3f2fc154737
seu_usuario <seu_nome@email.com>
2024-10-30 15:24:03 -0300

    atualizado

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# Cria uma lista e usuários e senha, depois vamos pegar no DB
+usuarios = {
+    'admin' : 'admin',
+    'usuario' : 'senha',
+    
+}
+
+@app.route('/') #rota para a página inicial
+def home():
+    return render_template('index.html')
+
+@app.route('/login') #rota para a página de login
+def login():
+    return render_template('login.html')
+
+# VERFIFICAR O LOGIN
+@app.route('/verificar-login', methods=['POST'])
+def verificar_login():
+# Pegando o que o usuário digitou no campo de entrada de user e senha
+    username = request.form['username']
+    password = request.form['password']
+
+    # Verifica se o usuario digitado está na lista e se 
+    # a senha está certa
+    if username in usuarios and usuarios[username] == password:
+        return f"Bem-vindo, {username}!"
+    else:
+        return "Usuário ou senha inválidos."
+
+
+
+
+
+
+# parte principal do
+if __name__ == '__main__':
+    app.run(debug=True)
